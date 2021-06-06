b92452effab94ab30c17c21e7ec50508922f2f1d
Vanherwegentim <Tim.vanherwegen@student.ucll.be>
2021-06-06 18:38:43 +0200

    big update

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,7 +63,7 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "Vanherwegentim");
+    new GitHubCalendar("#github-graph", "IonicaBizau");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
